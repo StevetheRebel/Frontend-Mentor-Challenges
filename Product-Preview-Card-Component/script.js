5331581513c0baa59e04bbb4aa19cecacfc3010c
Steve Mogan Odumbe <124534650+StevetheRebel@users.noreply.github.com>
2024-07-02 23:11:42 +0300

    changes to the styling and script

--- Product-Preview-Card-Component/script.js
+++ Product-Preview-Card-Component/script.js
@@ -1,20 +1,5 @@
 const btn = document.getElementById("btn");
-btn.style.backgroundColor = "var(--pri-darkCyan)";
 
-function activeBtn() {
-  btn.style.backgroundColor = "var(--pri-darkerCyan)";
-}
-
-function resetBtn() {
-  btn.style.backgroundColor = "var(--pri-darkCyan)";
-}
-
-function btnPress () {
-	if (btn.style.backgroundColor == "var(--pri-darkCyan)") {
-		activeBtn()
-	} else {
-		resetBtn()
-	}
-}
-
-btn.addEventListener('click', btnPress)
+btn.addEventListener("click", () => {
+  btn.classList.toggle("active");
+});
