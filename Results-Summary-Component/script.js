043240608c635baa74430d26cba52cdc19d17594
Steve Mogan Odumbe <124534650+StevetheRebel@users.noreply.github.com>
2024-07-03 01:53:38 +0300

    changes to style and script and html tag names

--- Results-Summary-Component/script.js
+++ Results-Summary-Component/script.js
@@ -1,20 +1,5 @@
 const btn = document.getElementById('btn');
 
-
-function changeColor() {
-	btn.style.backgroundColor = 'var(--sec-grayBlue)'
-}
-
-function resetColor() {
-	btn.style.backgroundColor = 'var(--pri-cobaltBlue)'
-}
-
-function btnClick() {
-	if (btn.style.backgroundColor === 'var(--sec-grayBlue)') {
-		resetColor();
-	} else {
-		changeColor()
-	}
-}
-
-btn.addEventListener('click', btnClick)
+btn.addEventListener('click', () => {
+	btn.classList.toggle('active')
+})
