93aa5ac10e61ffe49ed6a20d22b08677c0da3d40
Steve Mogan Odumbe <124534650+StevetheRebel@users.noreply.github.com>
2025-05-22 21:59:06 +0300

    restart

--- product-list-with-cart/src/App.jsx
+++ product-list-with-cart/src/App.jsx
@@ -1,30 +1,9 @@
-import React, { useEffect, useState } from "react";
-import Cards from "./Components/Cards/Cards";
-import axios from "axios";
+import React from 'react'
 
 function App() {
-  const [productNames, setProductNames] = useState([]);
-  const [productCategory, setProductCategory] = useState([])
-
-  useEffect(() => {
-    axios
-      .get("./../../../data.json")
-      .then(
-        res => {
-          const names = res.data.map(product => product.name)
-          const category = res.data.map(cat => cat.name)
-          setProductCategory(category)
-          setProductNames(names)
-        }
-      )
-      .catch((err) => console.log(err));
-  }, []);
-
   return (
-    <div>
-      <Cards productNames={productNames} productCategory={productCategory} />
-    </div>
-  );
+    <div>App</div>
+  )
 }
 
-export default App;
+export default App
--- product-list-with-cart/src/Components/CartSummary/CartSummary.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-function CartSummary() {
-  return (
-    <div>CartSummary</div>
-  )
-}
-
-export default CartSummary
